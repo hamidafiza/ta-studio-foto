6236c61eaefe413c061626217a7ba8b9df702606
RALD <yr.alditya@gmail.com>
2022-07-12 22:08:22 +0700

    adminlte sparo

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/booking', [App\Http\Controllers\BookingController::class, 'index'])->name('booking');
+Route::get('/tes', function(){
+    return view('layouts.master');
+});
+
